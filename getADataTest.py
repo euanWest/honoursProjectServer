9776ea7cafa4d87d3256c4c12500982e4126aff8
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-29 15:57:59 +0000

    made a pie

---
+++ getADataTest.py
@@ -0,0 +1,12 @@
+# import fastf1
+import fastf1
+
+# Create session to get telemetry data
+session = fastf1.get_session(2023, 'Silverstone', 'Q')
+session.load(telemetry = True, laps = False, weather = False)
+
+# Get data on driver
+hamilton = session.get_driver('HAM')
+
+#Test print
+print(hamilton)
