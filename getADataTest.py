b6f9a4ddf0ef0112088190e138ce85cbfb5af640
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-29 16:55:09 +0000

    trying this now

--- getADataTest.py
+++ getADataTest.py
@@ -3,11 +3,10 @@ import fastf1
 
 # Create session to get telemetry data
 session = fastf1.get_session(2023, 'Silverstone', 'Q')
-session.load(telemetry = True, laps = False, weather = False)
+session.load()
 
 # Get data on driver
-hamilton = session.get_driver('HAM')
-hamCarData = hamilton.get_pos_data()
+hamilton = session.pick_driver('HAM').get_pos_data()
 
 #Test print
 print(hamCarData)
