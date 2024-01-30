1b7b0416cbb8616060ecdbffed662ef25096104d
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-30 12:03:32 +0000

    try writing to json

--- getADataTest.py
+++ getADataTest.py
@@ -1,5 +1,6 @@
-# import fastf1
+# import fastf1 and json
 import fastf1
+import json
 
 # Create session to get telemetry data
 session = fastf1.get_session(2023, 'Silverstone', 'Q')
@@ -8,5 +9,7 @@ session.load()
 # Get data on driver
 hamilton = session.laps.pick_driver('HAM').pick_fastest().get_pos_data()
 
-#Test print
+# Test print
 print(hamilton)
+with open('telData.json', 'w', encoding='utf-8') as f:
+    json.dump(hamilton, f, ensure_ascii=False, indent=4)
