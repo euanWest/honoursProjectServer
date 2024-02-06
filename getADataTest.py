eb1ba731acc0a924059505dbd3bbcf65e1dbab5f
euanWest <72396471+euanWest@users.noreply.github.com>
2024-02-06 12:26:40 +0000

    cast all to str

--- getADataTest.py
+++ getADataTest.py
@@ -15,7 +15,7 @@ telemetryData = []
 
 # Add data to new dictionary
 for i in range (0, 335):
-    telemetryData.append({'time' : str(hamilton.Time[i].total_seconds()), 'x' : hamilton.X[i], 'y' : hamilton.Y[i], 'z' : hamilton.Z[i]})
+    telemetryData.append({'time' : str(hamilton.Time[i].total_seconds()), 'x' : str(hamilton.X[i]), 'y' : str(hamilton.Y[i]), 'z' : str(hamilton.Z[i])})
 
 # Flask app to serve Json
 app = Flask(__name__)
