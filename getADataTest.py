6a8a5588e567ef646ae67fa6df6fdd2776585c74
euanWest <72396471+euanWest@users.noreply.github.com>
2024-02-06 12:22:44 +0000

    test this

--- getADataTest.py
+++ getADataTest.py
@@ -24,7 +24,7 @@ app.debug = True
 @app.route("/downloadTel")
 def updateCheck():
      # IF Change in github repo
-    return jsonify(json.dumps(telemetryData, indent=1))
+    return jsonify(telemetryData)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
